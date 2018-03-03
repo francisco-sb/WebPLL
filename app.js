c2f588a0fd81f7dd143dec5d2af970f287c6c26f
francisco-sb <fmsb0193@gmail.com>
2018-03-03 16:27:18 -0600

    Production

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var server = require('http').createServer(app);
+
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
@@ -9,4 +12,7 @@ app.get("/", function(request, response) {
   response.render("index");
 });
 
-app.listen(8080);
+//app.listen(8080);
+server.listen(port, function()) {
+  console.log('listening on *:' + port);
+}
